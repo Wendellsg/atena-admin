f8a8c6ac5ee49a1641ddf8505e3f040700fd6a21
Wendell Guimarães <wendell@asupernova.com.br>
2025-08-29 12:14:11 -0300

    feat: restructure routing to implement public and private routes with nested layouts

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import Router from "./router.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
+    aaa
     <Router />
   </StrictMode>
 );
--- src/router.tsx
+++ src/router.tsx
@@ -1,12 +1,37 @@
-import { BrowserRouter, Routes } from "react-router";
-import { PublicRoutes } from "./pages/(public)/route";
-
-const ROUTES: React.ReactNode[] = [<PublicRoutes />];
+import { BrowserRouter, Route, Routes } from "react-router";
+import { DashBoardLayout } from "./pages/(private)/dashboard/layout";
+import { DashboardPage } from "./pages/(private)/dashboard/page";
+import { UserPage } from "./pages/(private)/dashboard/users/[id]/page";
+import { UsersPage } from "./pages/(private)/dashboard/users/page";
+import { PrivateLayout } from "./pages/(private)/layout";
+import { PublicLayout } from "./pages/(public)/layout";
+import { LoginPage } from "./pages/(public)/login/page";
+import { LangePage } from "./pages/(public)/page";
+import { RegisterPage } from "./pages/(public)/register/page";
 
 function Router() {
   return (
     <BrowserRouter>
-      <Routes>{ROUTES.map((route) => route)}</Routes>
+      aa
+      <Routes>
+        <Route path="/" element={<PublicLayout />}>
+          <Route index element={<LangePage />} />
+          <Route path="login" element={<LoginPage />} />
+          <Route path="register" element={<RegisterPage />} />
+        </Route>
+
+        <Route element={<PrivateLayout />}>
+          <Route path="/dashboard" element={<DashBoardLayout />}>
+            <Route index element={<DashboardPage />} />
+            <Route path="users/*">
+              <Route path="/dashboard/users">
+                <Route index element={<UsersPage />} />
+                <Route path=":id" element={<UserPage />} />
+              </Route>
+            </Route>
+          </Route>
+        </Route>
+      </Routes>
     </BrowserRouter>
   );
 }
