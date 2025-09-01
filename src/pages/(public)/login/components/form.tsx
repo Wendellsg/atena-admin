99e79367d993263c6a60ab287652426ac0c175bb
Wendell Guimarães <wendell@asupernova.com.br>
2025-09-01 11:27:19 -0300

    feat: implement login form with validation and toggle password visibility

--- src/pages/(public)/login/components/form.tsx
+++ src/pages/(public)/login/components/form.tsx
@@ -1,5 +1,21 @@
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Eye, EyeClosed } from "lucide-react";
+import { useForm } from "react-hook-form";
 import { z } from "zod";
 
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { cn } from "@/lib/utils";
+import { useState } from "react";
 const loginFormSchema = z.object({
   document: z
     .string()
@@ -9,9 +25,84 @@ const loginFormSchema = z.object({
     .max(11, {
       message: "O documento deve ter 11 caracteres.",
     }),
-  password: z.string().min(6, {
-    message: "A senha deve ter pelo menos 6 caracteres.",
-  }).max(100, {
-    message: "A senha deve ter no máximo 100 caracteres.",
-  }),
+  password: z
+    .string()
+    .min(6, {
+      message: "A senha deve ter pelo menos 6 caracteres.",
+    })
+    .max(100, {
+      message: "A senha deve ter no máximo 100 caracteres.",
+    }),
 });
+
+export type LoginFormValues = z.infer<typeof loginFormSchema>;
+
+export function LoginForm({
+  onSubmit,
+  className,
+}: {
+  onSubmit: (data: LoginFormValues) => void;
+  className?: string;
+}) {
+  const [showPassword, setShowPassword] = useState(false);
+
+  const form = useForm({
+    resolver: zodResolver(loginFormSchema),
+    defaultValues: {
+      document: "",
+      password: "",
+    },
+  });
+
+  return (
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className={cn("space-y-8", className)}
+      >
+        <FormField
+          control={form.control}
+          name="document"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>CPF | CNPJ</FormLabel>
+              <FormControl>
+                <Input placeholder="CPF | CNPJ" {...field} />
+              </FormControl>
+              <FormDescription>Informe seu CPF ou CNPJ.</FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={form.control}
+          name="password"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Senha</FormLabel>
+              <FormControl>
+                <div className="flex item-center gap-2">
+                  <Input
+                    type={showPassword ? "text" : "password"}
+                    placeholder="Sua senha"
+                    {...field}
+                  />
+                  <Button
+                    type="button"
+                    onClick={() => setShowPassword((prev) => !prev)}
+                  >
+                    {showPassword ? <EyeClosed /> : <Eye />}
+                  </Button>
+                </div>
+              </FormControl>
+              <FormDescription>Informe sua senha.</FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit">Enviar</Button>
+      </form>
+    </Form>
+  );
+}
